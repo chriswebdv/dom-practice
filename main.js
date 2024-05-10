1a3630802c1072e3ad9d03fa0fa19d286bc91dee
chriswebdv <chriswebdv@gmail.com>
2024-05-10 13:02:36 -0600

    Create h3 element via javascript DOM

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const para = document.createElement("p");
+para.classList.add("para");
+para.textContent = "Hey I’m red!";
+para.style.color = "red";
+
+container.appendChild(para);
+
+const h3Element = document.createElement("h3");
+h3Element.classList.add("h3Element");
+h3Element.textContent = "I’m a blue h3!";
+h3Element.style.color = "blue";
+
+container.appendChild(h3Element);
+
