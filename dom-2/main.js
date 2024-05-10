963a51b44660057cd595084348f7a7628e0556f4
chriswebdv <chriswebdv@gmail.com>
2024-05-10 17:07:18 -0600

    Create DOM elements to add to my page and custom inline styles

--- dom-2/main.js
+++ dom-2/main.js
@@ -6,8 +6,15 @@ const sect = document.querySelector("section");
 
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
+para.style.color = "white";
+para.style.backgroundColor = "black";
+para.style.padding = "10px";
+para.style.width = "250px";
+para.style.textAlign = "center";
 
 sect.appendChild(para);
+
+
 
 const text = document.createTextNode(
     " — the premier source for web development knowledge.",
@@ -15,4 +22,9 @@ const text = document.createTextNode(
 
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
+
+
+
+sect.appendChild(linkPara);
+
   
