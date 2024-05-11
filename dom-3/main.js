27c0a7c9d4ee8c768ec9bd5a605bd0b724ee66ef
chriswebdv <chriswebdv@gmail.com>
2024-05-11 10:04:39 -0600

    Add new div and inside added p and h1 elements using DOM model, add styles and append them all to the main container

--- dom-3/main.js
+++ dom-3/main.js
@@ -13,3 +13,20 @@ h3Element.textContent = "I'm a blue h3";
 h3Element.style.cssText = "color: blue";
 
 container.appendChild(h3Element);
+
+const newDiv = document.createElement("div");
+newDiv.classList.add("div");
+newDiv.style.backgroundColor = "pink";
+newDiv.style.border = "3px solid black";
+
+const h1Element = document.createElement("h1");
+h1Element.classList.add("h1Element");
+h1Element.textContent = "I'm in a div";
+newDiv.appendChild(h1Element);
+
+const para2 = document.createElement("p");
+para2.classList.add("para2");
+para2.textContent = "ME TOO!";
+newDiv.appendChild(para2);
+
+container.appendChild(newDiv);
